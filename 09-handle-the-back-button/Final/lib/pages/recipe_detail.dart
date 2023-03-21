eca1bdaa1262382982381055c753e0d4a7146d2f
Yogesh Choudhary <youzendachoudhary22@gmail.com>
2023-03-21 18:36:17 +0530

    09-handle-back-button

--- 09-handle-the-back-button/Final/lib/pages/recipe_detail.dart
+++ 09-handle-the-back-button/Final/lib/pages/recipe_detail.dart
@@ -96,106 +96,115 @@ class _RecipeDetailState extends State<RecipeDetail>
   }
 
   void _navigateBack(BuildContext context) {
-    Navigator.pop(context);
+    _show.reverse();
+    Future.delayed(const Duration(milliseconds: 360), () {
+      Navigator.pop(context);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios, size: 28),
-          onPressed: () {
-            _navigateBack(context);
-          },
-        ),
-        actions: const <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(0, 8, 24, 8),
-            child: Icon(
-              FontAwesomeIcons.heart,
-              size: 28,
-            ),
-          )
-        ],
-      ),
-      bottomNavigationBar: SizeTransition(
-        sizeFactor: _navAnimation,
-        axisAlignment: -1,
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: navItems
-              .map((item) => BottomNavigationBarItem(
-                    icon: Icon(item.icon),
-                    label: item.title,
-                  ))
-              .toList(),
-          currentIndex: 0,
-          backgroundColor: const Color(0xFFf0efed),
-        ),
-      ),
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints viewportConstraints) {
-          return SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight: viewportConstraints.maxHeight,
+    return WillPopScope(
+      onWillPop: () async {
+        _navigateBack(context);
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios, size: 28),
+            onPressed: () {
+              _navigateBack(context);
+            },
+          ),
+          actions: const <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 8, 24, 8),
+              child: Icon(
+                FontAwesomeIcons.heart,
+                size: 28,
               ),
-              child: IntrinsicHeight(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      // A fixed-height child.
-                      height: 200,
-                      child: Header(recipe: widget.recipe),
-                    ),
-                    Expanded(
-                      // A flexible child that will grow to fit the viewport but
-                      // still be at least as big as necessary to fit its contents.
-                      child: Stack(
-                        alignment: Alignment.topCenter,
-                        children: <Widget>[
-                          HeroBackground(recipe: widget.recipe),
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                transform:
-                                    Matrix4.translationValues(0.0, -30.0, 0.0),
-                                child: HeroRecipeImage(
-                                  imagePath: widget.recipe.imagePath,
-                                ),
-                              ),
-                              AnimatedBuilder(
-                                animation: _show,
-                                builder: (context, child) {
-                                  return SlideTransition(
-                                    position: _contentAnimation,
-                                    child: Opacity(
-                                      opacity: _show.value,
-                                      child: child,
-                                    ),
-                                  );
-                                },
-                                child: Column(
-                                  children: <Widget>[
-                                    IngredientList(
-                                      ingredients: widget.recipe.ingredients,
-                                    ),
-                                    // Other contents can be added here
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+            )
+          ],
+        ),
+        bottomNavigationBar: SizeTransition(
+          sizeFactor: _navAnimation,
+          axisAlignment: -1,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            items: navItems
+                .map((item) => BottomNavigationBarItem(
+                      icon: Icon(item.icon),
+                      label: item.title,
+                    ))
+                .toList(),
+            currentIndex: 0,
+            backgroundColor: const Color(0xFFf0efed),
+          ),
+        ),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: IntrinsicHeight(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        // A fixed-height child.
+                        height: 200,
+                        child: Header(recipe: widget.recipe),
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        // A flexible child that will grow to fit the viewport but
+                        // still be at least as big as necessary to fit its contents.
+                        child: Stack(
+                          alignment: Alignment.topCenter,
+                          children: <Widget>[
+                            HeroBackground(recipe: widget.recipe),
+                            Column(
+                              children: <Widget>[
+                                Container(
+                                  transform: Matrix4.translationValues(
+                                      0.0, -30.0, 0.0),
+                                  child: HeroRecipeImage(
+                                    imagePath: widget.recipe.imagePath,
+                                  ),
+                                ),
+                                AnimatedBuilder(
+                                  animation: _show,
+                                  builder: (context, child) {
+                                    return SlideTransition(
+                                      position: _contentAnimation,
+                                      child: Opacity(
+                                        opacity: _show.value,
+                                        child: child,
+                                      ),
+                                    );
+                                  },
+                                  child: Column(
+                                    children: <Widget>[
+                                      IngredientList(
+                                        ingredients: widget.recipe.ingredients,
+                                      ),
+                                      // Other contents can be added here
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
